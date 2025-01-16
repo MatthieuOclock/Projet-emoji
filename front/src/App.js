3f0c39c7fc8b626878783483253e1aaf4c29656a
Flatcher <mslmatth@gmail.com>
2025-01-16 14:49:20 +0400

    add front dependance

---
+++ front/src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
+import { ToastContainer } from "react-toastify";
+
+const App = () => { 
+    return( 
+        <Routes>
+            
+        </Routes>
+    )
+}
+
+export default App; 
